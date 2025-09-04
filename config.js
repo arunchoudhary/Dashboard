90bb8647363807d32ee141ee5faa15871e39e9fa
arunchoudhary <43253750+arunchoudhary@users.noreply.github.com>
2025-09-04 22:37:45 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDVCL7dAxL0N-wAuqNTpdSd2yRXKUf0cz0",
+  authDomain: "dashboard-84cb9.firebaseapp.com",
+  projectId: "dashboard-84cb9",
+  storageBucket: "dashboard-84cb9.appspot.com",
+  messagingSenderId: "1032068257985",
+  appId: "1:1032068257985:web:de6b66c067b8c8d5111c40"
+};
